037f85a04d72ec7a91cb7574c27796873eb3efc1
SnowSurfer <drinkaziu@gmail.com>
2018-12-18 17:24:21 -0500

    add REVERSE

--- data_structures/singly_linked_lists/singly_linked_list.js
+++ data_structures/singly_linked_lists/singly_linked_list.js
@@ -127,6 +127,23 @@ class SinglyLinkedList {
     this.length--
     return nodeToRemove;
   }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for(let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this; 
+  }
 }
 
 // let list = new SinglyLinkedList();
