ff4e7a62b6152adb6755fad1a1f6e77d2eb5ee31
SnowSurfer <drinkaziu@gmail.com>
2018-12-18 11:05:41 -0500

    radix sort

---
+++ intermediate_sorting/radix_sort.js
@@ -0,0 +1,35 @@
+// get the digit at a given position
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+
+// How many digits in a number?(or you can convert to string and get the length)
+function digitCount(num) {
+  if(num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// Find the most digits a number in the array of numbers has
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for(let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    maxDigits = Math.max(maxDigits, digitCount(num));
+  }
+
+  return maxDigits;
+}
+
+// Radix Sort
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  for(let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({length: 10}, () => []); //create a bucket(array) of 10 empty arrays
+    for(let i = 0; i < nums.length; i++) {
+      let num = nums[i];
+      let digit = getDigit(num, k)
+      digitBuckets[digit].push(num);
+    }
+  }
+}
