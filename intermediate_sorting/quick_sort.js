51e5dfc79f022c1d71ba4edbf909e008916046c7
SnowSurfer <drinkaziu@gmail.com>
2018-12-18 09:25:19 -0500

    quick sort

---
+++ intermediate_sorting/quick_sort.js
@@ -0,0 +1,27 @@
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for(let i = start + 1; i < arr.length; i++) {
+    if(pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if(left < right) {
+    let pivotIdx = pivot(arr, left, right);
+    quickSort(arr, left, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, right);
+  }
+
+  return arr;
+}
+
+quickSort([4, 8, 2, 1, 5, 7, 6, 3]);
