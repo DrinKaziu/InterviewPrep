66826241508fba898eca5cfdebf5c375f6dcaed8
SnowSurfer <drinkaziu@gmail.com>
2018-12-21 10:11:23 -0500

    stacks & queues

---
+++ data_structures/stacks_queues/array_stack.js
@@ -0,0 +1,11 @@
+let stack = [];
+
+stack.push('google');
+stack.push('instagram');
+stack.push('youtube');
+
+stack.pop();
+
+//we can also use shift/unshift but adding and removing
+//from the start of an array is not efficient sice we
+//have to re-index every element.
