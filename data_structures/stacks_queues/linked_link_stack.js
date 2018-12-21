aa7fe7b0a2f7709f793769cd85b52c3251ed2ffb
SnowSurfer <drinkaziu@gmail.com>
2018-12-21 11:23:43 -0500

    add PUSH & POP

---
+++ data_structures/stacks_queues/linked_link_stack.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  //We use shift/unshift but call it push/pop since push/pop is
+  //inefficient in sinly linked lists
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+
+    return ++this.size;
+  }
+
+  pop() {
+    if(!this.first) return null; 
+    let temp = this.first;
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp;
+  }
+}
