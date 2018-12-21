df74a7f8f872f87c0827df7d627848b7615be614
SnowSurfer <drinkaziu@gmail.com>
2018-12-21 16:23:55 -0500

    add DFS Pre-order

--- data_structures/binary_search_trees/bst.js
+++ data_structures/binary_search_trees/bst.js
@@ -77,4 +77,17 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPreOrder() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
