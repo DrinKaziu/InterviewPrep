5caf5daf56bbcfa53f94fdbbc3648ca1e97ed13e
SnowSurfer <drinkaziu@gmail.com>
2018-12-21 16:26:13 -0500

    add DFS Post-order

--- data_structures/binary_search_trees/bst.js
+++ data_structures/binary_search_trees/bst.js
@@ -90,4 +90,17 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+
+  DFSPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
